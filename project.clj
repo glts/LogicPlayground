fdd36e815e513c21060b47bde26a65f709879326
glts <676c7473@gmail.com>
2013-10-09 20:36:11 +0200

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject logic-playground "0.0.0-SNAPSHOT"
+  :description "Dummy project file for logic-playground"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.logic "0.8.4"]])
