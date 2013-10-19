99bcef48f4d45fd526fdf63c7d1215383418fe59
glts <676c7473@gmail.com>
2013-10-19 21:24:40 +0200

    Read chapter 2

--- reasoned/mini.scm
+++ reasoned/mini.scm
@@ -11,12 +11,34 @@
 
 ;; Chapter 2
 
+(define conso
+  (lambda (a d l)
+    (== (cons a d) l)))
+
 (define caro
   (lambda (l a)
     (fresh (d)
-      (== (cons a d) l))))
+      (conso a d l))))  ; was (== (cons a d) l)
 
 (define cdro
   (lambda (l d)
     (fresh (a)
-      (== (cons a d) l))))
+      (conso a d l))))
+
+(define nullo
+  (lambda (l)
+    (== '() l)))
+
+(define eqo
+  (lambda (x y)
+    (== x y)))
+; or rather: (define eqo ==)
+
+; memento:
+; (cons 'a (cons 'b '()))   => (a b), a proper list
+; (cons 'a (cons 'b 'c))    => (a b . c), an improper list
+
+(define pairo
+  (lambda (p)
+    (fresh (a d)
+      (conso a d p))))
